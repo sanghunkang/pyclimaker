63893a43c7c6b00866904f590cb534402d1e77f6
sanghunkang <sanghunkang.dev@gmail.com>
2019-09-13 23:59:45 +0900

    added argprompt iterface

--- example.py
+++ example.py
@@ -1,8 +1,18 @@
+from pyclimaker import PyCliFunctionPrompt
+
+def foo(x, following_message="the following message"):
+    print(f"This function prints the input:{x} with {following_message}")
 
 
 
-command_foo = PyCLICommand()
-prompt_arg_foo = PyCLIArgPrompt(
-    message=
+
+command_foo = PyCliFunctionPrompt(
+    function= foo,
+    description="This is the foo function"
 )
-command_foo.add_arg_prompt(prompt_arg_foo)
+# command_foo.add_arg_prompt(prompt_arg_foo)
+
+
+# prompt_arg_foo = PyCLIArgPrompt(
+#     message=
+# )
--- pyclimaker/__init__.py
+++ pyclimaker/__init__.py
@@ -1,4 +1,4 @@
-class PyCLIMain():
+class PyCliMain():
     def __init__(self):
         self.MESSAGE_PROMPT = "Enter command you want to execute: "
         self.MESSAGE_PROMPT_INVALID_COMMAND = "This is an invalid key"
@@ -9,7 +9,7 @@ class PyCLIMain():
         }
     
     def add_cli_command(self, cli_command):
-        if isinstance(cli_command, PyCLICommand):
+        if isinstance(cli_command, PyCliFunctionPrompt):
             self.cli_commmands[cli_command.command] = cli_command
         else:
             raise Exception
@@ -28,44 +28,33 @@ class PyCLIMain():
             else:
                 print(self.MESSAGE_PROMPT_INVALID_COMMAND)
 
-            
-
-
-
-
-class PyCLICommand():
-    def __init__(self, function):
-        # Define command name
-        # Add help message
-        self.command = ""
-        self.help_message = ""
-        
-        self.function = None
-        self.function_args = {} # Can be either defined by developers or inputted by users
-        self.function_args_aliases = {}
-        self.valid_inputs = []
-
-        # Initialisation with internal methods 
-        self.bind_function(function)
-
-    def bind_function(self, function):
+class PyCliFunctionPrompt():
+    def __init__(self, function, description):
         if callable(function) == False: # What really matters is if some variable does some operations
             raise Exception("Invalid argument. function must be a type of callable.")
         
+        argcount_non_defaults = function.__code__.co_argcount - len(function.__defaults__)
+        
+        # Initialisation
         self.function = function
-        self.function_args = {}
-        argcount_non_defaults = len(function.__code__.co_argcount - function.__defaults__)
-        for index in range(function.__code__.co_argcount):
-            key = function.__code__.co_varnames[index]
-            value_default_at_function = argcount_non_defaults <= index? function.__defaults__[index]: None
-
-            self.function_args[key] = {
+        self.function_args = {} # Can be either defined by developers or inputted by users
+        for i in range(function.__code__.co_argcount):
+            if argcount_non_defaults < i: #function.__defaults__[i]  else None
+                default_at_function = function.__defaults__[i]
+            else:
+                default_at_function = None
+            
+            self.function_args[function.__code__.co_varnames[i]] = {
                 "is_predefined": False,
                 "default_at_cli": None,
-                "default_at_function": value_default_at_function
+                "default_at_function": default_at_function
             }
 
         self.function_args_aliases = {key: key for key in self.function_args.keys()}
+        self.description = description
+        
+        # Properties to be filled by the developer
+        self.arg_prompts = {}
 
     def bind_function_args_aliases(self, alias, arg_name):
         if isinstance(alias, str) == False:
@@ -82,31 +71,46 @@ class PyCLICommand():
         self.function_args[arg_name].arg_value = default_at_cli
         
     def bind_arg_prompt(self, arg_prompt, command_to_bind):
-        if isinstance(arg_prompt, PyCLIArgPrompt) == False:
-            raise Exception("Invalid argument. arg_prompt must be a type of PyCLIArgPrompt (or its inheritance?)")
+        if isinstance(arg_prompt, PyCliArgPrompt) == False:
+            raise Exception("Invalid argument. arg_prompt must be a type of PyCliArgPrompt (or its inheritance?)")
         
         if isinstance(command_to_bind, str) == False:
             # NOTE MAYBE I CAN LOOSEN THE RESTIRCTION TO ACCEPT TYPES LIKE INTS
             raise Exception()
             
-        if command_to_bind not in self.valid_commands:
+        if command_to_bind not in self.function_args_aliases:
             # Any input that doesn't contribute to specifying argument to the function is considered invalid
 
             raise Exception("Invalid binding. ")
 
+        if self.function_args_aliases[command_to_bind] in self.arg_prompts:
+            raise Exception("Overring arg_prompt")
 
+        self.arg_prompts[command_to_bind] = arg_prompt
 
-    def execute(self):
+    def trigger(self):
         # Do base prompting
+        args_to_pass = {}
+        
+        # Prepare arguments to pass using CLI defaults and functions defaults
+        for arg_name, arg_value in self.function_args.items():
+            if arg_value["default_at_cli"] != None:
+                arg_value_to_pass = arg_value["default_at_cli"]
+            elif arg_value["default_at_function"] != None:
+                arg_value_to_pass = arg_value["default_at_function"]
+            else:
+                arg_value_to_pass = None
 
-        # if input which triggers binded action is givern
-        for prompt in self.prompts:
-            prompt.execute
+            args_to_pass[arg_name] = arg_value_to_pass
 
-        pass
+        # Prepare arguments using user inputs
+        for arg_name, arg_prompt in self.arg_prompts.items():
+            args_to_pass[arg_name] = arg_prompt.trigger()
+
+        self.function(**args_to_pass)
 
 
-class PyCLIArgPrompt():
+class PyCliArgPrompt():
     def __init__(self, message):
         self.message = message
     
@@ -114,3 +118,4 @@ class PyCLIArgPrompt():
         # Do base prompting
 
         # if input which triggers binded action is givern
+        pass
